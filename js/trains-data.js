ac260649b851b2aa2f5ec2ea3a5c81356e17382f
shashankism3 <shashankmadhukar2@gmail.com>
2026-05-24 00:09:39 +0530

    Add files via upload

--- js/trains-data.js
+++ js/trains-data.js
@@ -7,13 +7,159 @@ const amritBharatTrains = [
         inauguration: "14 May 2026",
         link: "trains/14663-new-jalpaiguri-amritsar-amrit-bharat-express.html"
     },
-    {
+	{
         number: "14664",
         name: "Amritsar - New Jalpaiguri Amrit Bharat Express",
         inauguration: "14 May 2026",
         link: "trains/14664-amritsar-new-jalpaiguri-amrit-bharat-express.html"
     },
-	 {
+	{   number: "22111",
+        name: "Lokmanya Tilak - Ayodhya Cantt. Amrit Bharat Express",
+        inauguration: "28 April 2026",
+        link: "#"
+    },
+	{
+        number: "22112",
+        name: "Ayodhya Cantt. - Lokmanya Tilak Amrit Bharat Express",
+        inauguration: "28 April 2026",
+        link: "#"
+    },
+	{   number: "22589",
+        name: "Banaras – Hadapsar(Pune) Amrit Bharat Express",
+        inauguration: "28 April 2026",
+        link: "#"
+    },
+	{
+        number: "22590",
+        name: "Hadapsar(Pune) - Banaras Amrit Bharat Express",
+        inauguration: "28 April 2026",
+        link: "#"
+    },
+	{   number: "17065",
+        name: "Charlapalli – Shalimar Amrit Bharat Express",
+        inauguration: "07 April 2026",
+        link: "#"
+    },
+	{
+        number: "17066",
+        name: "Shalimar – Charlapalli  Amrit Bharat Express",
+        inauguration: "07 April 2026",
+        link: "#"
+    },
+	{   number: "15673",
+        name: "Charlapalli – Kamakhya Amrit Bharat Express",
+        inauguration: "13 March 2026",
+        link: "#"
+    },
+	{
+        number: "15674",
+        name: "Kamakhya – Charlapalli  Amrit Bharat Express",
+        inauguration: "13 March 2026",
+        link: "#"
+    },
+	{   number: "16357",
+        name: "Nagercoil – Cherlapalli Amrit Bharat Express",
+        inauguration: "11 March 2026",
+        link: "#"
+    },
+	{
+        number: "16358",
+        name: "Charlapalli - Nagercoil Amrit Bharat Express",
+        inauguration: "11 March 2026",
+        link: "#"
+    },
+	{   number: "16619",
+        name: "Podanur – Dhanbad Amrit Bharat Express",
+        inauguration: "11 March 2026",
+        link: "#"
+    },
+	{
+        number: "16620",
+        name: "Dhanbad - Podanur Amrit Bharat Express",
+        inauguration: "11 March 2026",
+        link: "#"
+    },
+    {   number: "11031",
+        name: "Panvel – Alipurduar Amrit Bharat Express",
+        inauguration: "23 February 2026",
+        link: "#"
+    },
+	{
+        number: "11032",
+        name: "Alipurduar – Panvel Amrit Bharat Express",
+        inauguration: "23 February 2026",
+        link: "#"
+    },
+	{    number: "16329",
+        name: "Nagercoil Junction – Mangaluru Junction Amrit Bharat Express",
+        inauguration: "23 January 2026",
+        link: "#"
+    },
+	{
+        number: "17041",
+        name: "Charlapalli – Thiruvananthapuram North Amrit Bharat Express",
+        inauguration: "23 January 2026",
+        link: "#"
+    },
+	{    number: "17042",
+        name: "Thiruvananthapuram North – Charlapalli Amrit Bharat Express",
+        inauguration: "23 January 2026",
+        link: "#"
+    },
+	{
+        number: "16330",
+        name: "Mangaluru Junction – Nagercoil Junction Amrit Bharat Express",
+        inauguration: "23 January 2026",
+        link: "#"
+    },
+	{    number: "16121",
+        name: "Thiruvananthapuram Central - Tambaram Amrit Bharat Express",
+        inauguration: "23 January 2026",
+        link: "#"
+    },
+	{
+        number: "16122",
+        name: "Tambaram – Thiruvananthapuram Central Amrit Bharat Express",
+        inauguration: "23 January 2026",
+        link: "#"
+    },
+	{
+        number: "13065",
+        name: "Howrah – Anand Vihar Amrit Bharat Express",
+        inauguration: "18 January 2026",
+        link: "#"
+    },
+	{
+        number: "13066",
+        name: "Anand Vihar - Howrah Amrit Bharat Express",
+        inauguration: "18 January 2026",
+        link: "#"
+    },
+   	{
+        number: "15949",
+        name: "Dibrugarh – Gomti Nagar Amrit Bharat Express",
+        inauguration: "18 January 2026",
+        link: "#"
+    },
+	{
+        number: "15950",
+        name: "Gomti Nagar – Dibrugarh Amrit Bharat Express",
+        inauguration: "18 January 2026",
+        link: "#"
+    },
+	{
+        number: "15671",
+        name: "Kamakhya – Rohtak Amrit Bharat Express",
+        inauguration: "18 January 2026",
+        link: "#"
+    },
+	{
+        number: "15672",
+        name: "Rohtak - Kamakhya Amrit Bharat Express",
+        inauguration: "18 January 2026",
+        link: "#"
+    },
+	{
         number: "16107",
         name: "Tambaram - Santragachi Amrit Bharat Express",
         inauguration: "18 January 2026",
@@ -26,15 +172,27 @@ const amritBharatTrains = [
         link: "#"
     },
 	{
-        number: "15949",
-        name: "Dibrugarh – Gomti Nagar Amrit Bharat Express",
+        number: "22587",
+        name: "Sealdah – Banaras Amrit Bharat Express",
+        inauguration: "18 January 2026",
+        link: "#"
+    },
+    {
+        number: "22588",
+        name: "Banaras - Sealdah Amrit Bharat Express",
         inauguration: "18 January 2026",
         link: "#"
     },
 	{
-        number: "15950",
-        name: "Gomti Nagar – Dibrugarh Amrit Bharat Express",
-        inauguration: "18 January 2026",
+        number: "16597",
+        name: "SMVT Bengaluru – Alipurduar Amrit Bharat Express",
+        inauguration: "17 January 2026",
+        link: "#"
+    },
+	{
+        number: "16598",
+        name: "Alipurduar - SMVT Bengaluru Amrit Bharat Express",
+        inauguration: "17 January 2026",
         link: "#"
     },
 	{
@@ -62,15 +220,147 @@ const amritBharatTrains = [
         link: "#"
     },
 	{
-        number: "16597",
-        name: "SMVT Bengaluru – Alipurduar Amrit Bharat Express",
-        inauguration: "17 January 2026",
+        number: "15133",
+        name: "Chhapra – Anand Vihar Terminal Amrit Bharat Express",
+        inauguration: "26 October 2025",
         link: "#"
     },
 	{
-        number: "16598",
-        name: "Alipurduar - SMVT Bengaluru Amrit Bharat Express",
-        inauguration: "17 January 2026",
+        number: "15134",
+        name: "Anand Vihar Terminal - Chhapra Amrit Bharat Express",
+        inauguration: "29 September 2025",
+        link: "#"
+    },
+	{
+        number: "15293",
+        name: "Muzaffarpur – Charlapalli Amrit Bharat Express",
+        inauguration: "26 October 2025",
+        link: "#"
+    },
+	{
+        number: "15294",
+        name: "Muzaffarpur – Charlapalli Amrit Bharat Express",
+        inauguration: "29 September 2025",
+        link: "#"
+    },
+	{
+        number: "14627",
+        name: "Saharsa – Chheharta (Amritsar) Amrit Bharat Express",
+        inauguration: "29 September 2025",
+        link: "#"
+    },
+	{
+        number: "14628",
+        name: "Chheharta (Amritsar) - Saharsa Amrit Bharat Express",
+        inauguration: "29 September 2025",
+        link: "#"
+    },
+	{
+        number: "19623",
+        name: "Madar (Ajmer)– Darbhanga Amrit Bharat Express",
+        inauguration: "29 September 2025",
+        link: "#"
+    },
+	{
+        number: "19624",
+        name: "Darbhanga - Madar (Ajmer) Amrit Bharat Express",
+        inauguration: "29 September 2025",
+        link: "#"
+    },
+	{
+        number: "19021",
+        name: "Udhna (Surat) – Brahmapur Amrit Bharat Express",
+        inauguration: "27 September 2025",
+        link: "#"
+    },
+	{
+        number: "19022",
+        name: "Brahmapur – Udhna(Surat) Amrit Bharat Express",
+        inauguration: "27 September 2025",
+        link: "#"
+    },
+	{
+        number: "16601",
+        name: "Erode - Jogbani Amrit Bharat Express",
+        inauguration: "15 September 2025",
+        link: "#"
+    },
+	{
+        number: "16602",
+        name: "Jogbani - Erode Amrit Bharat Express",
+        inauguration: "15 September 2025",
+        link: "#"
+    },
+	{
+        number: "14047",
+        name: "Sitamarhi - Delhi Amrit Bharat Express",
+        inauguration: "22 August 2025",
+        link: "#"
+    },
+	{
+        number: "14048",
+        name: "Delhi - Sitamarhi Amrit Bharat Express",
+        inauguration: "22 August 2025",
+        link: "#"
+    },
+	{
+        number: "13697",
+        name: "Gaya - Delhi Amrit Bharat Express",
+        inauguration: "22 August 2025",
+        link: "#"
+    },
+	{
+        number: "13698",
+        name: "Delhi - Gaya Amrit Bharat Express",
+        inauguration: "22 August 2025",
+        link: "#"
+    },
+	{
+        number: "13435",
+        name: "Gomti Nagar – Malda Town Amrit Bharat Express",
+        inauguration: "18 July 2025",
+        link: "#"
+    },
+    {
+        number: "13436",
+        name: "Malda Town - Gomti Nagar Amrit Bharat Express",
+        inauguration: "18 July 2025",
+        link: "#"
+    },	
+	{
+        number: "15561",
+        name: "Darbhanga - Gomti Nagar Amrit Bharat Express",
+        inauguration: "18 July 2025",
+        link: "#"
+    },
+    {
+        number: "15562",
+        name: "Gomti Nagar - Darbhanga Amrit Bharat Express",
+        inauguration: "18 July 2025",
+        link: "#"
+    },
+	{
+        number: "15567",
+        name: "Bapudham Motihari - Anand Vihar Terminal Amrit Bharat Express",
+        inauguration: "18 July 2025",
+        link: "#"
+    },
+    {
+        number: "15568",
+        name: "Anand Vihar Terminal - Bapudham Motihari Amrit Bharat Express",
+        inauguration: "18 July 2025",
+        link: "#"
+    },
+	{
+        number: "22361",
+        name: "Rajendra Nagar Terminal – New Delhi Amrit Bharat Express",
+        inauguration: "18 July 2025",
+        link: "#"
+    },
+    {
+        number: "22362",
+        name: "New Delhi - Rajendra Nagar Terminal Amrit Bharat Express",
+        inauguration: "18 July 2025",
         link: "#"
     },
     {
@@ -94,6 +384,18 @@ const amritBharatTrains = [
     {
         number: "13434",
         name: "Malda Town - SMVT Bengaluru Amrit Bharat Express",
+        inauguration: "30 December 2023",
+        link: "#"
+    },
+	{
+        number: "15557",
+        name: "Darbhanga - Anand Vihar Amrit Bharat Express",
+        inauguration: "30 December 2023",
+        link: "#"
+    },
+    {
+        number: "15558",
+        name: "Anand Vihar - Darbhanga Amrit Bharat Express",
         inauguration: "30 December 2023",
         link: "#"
     }
